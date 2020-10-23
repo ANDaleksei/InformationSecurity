382262a8c26123390ea169efc22c4d8aee9bfb47
ANDaleksei <ANDaleksei@gmail.com>
2020-10-23 23:20:49 +0300

    implement sha256

--- lab3/CryptographicHashFunctions/CryptographicHashFunctions/main.swift
+++ lab3/CryptographicHashFunctions/CryptographicHashFunctions/main.swift
@@ -7,5 +7,12 @@
 
 import Foundation
 
-print("Hello, World!")
+let sha256 = SHA256()
+let data = sha256.hash(data: Array(repeating: "a", count: 1000000).joined().data(using: .utf8)!)
+for byte in data {
+  print(String(format: "%02x", byte), terminator: "")
+}
+print()
 
+testSHA256_case1()
+testSHA256_case2()
